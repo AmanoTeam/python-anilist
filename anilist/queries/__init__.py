015b12e54f96acc1efd06a7f494b358526f8e803
Nathaniel Fitzenrider <nathaniel.fitzenrider@gmail.com>
2021-10-21 00:46:38 -0400

    Black reformatting

--- anilist/queries/__init__.py
+++ anilist/queries/__init__.py
@@ -22,28 +22,28 @@ __all__ = (
     "TEXT_ACTIVITY_QUERY",
     "MESSAGE_ACTIVITY_QUERY",
     "MESSAGE_ACTIVITY_QUERY_SENT",
-    "MESSAGE_ACTIVITY_SENT_QUERY"
+    "MESSAGE_ACTIVITY_SENT_QUERY",
 )
 
-ANIME_SEARCH_QUERY = read_text(search, 'anime_search.graphql')
-MANGA_SEARCH_QUERY = read_text(search, 'manga_search.graphql')
-CHARACTER_SEARCH_QUERY = read_text(search, 'character_search.graphql')
-STAFF_SEARCH_QUERY = read_text(search, 'staff_search.graphql')
-USER_SEARCH_QUERY = read_text(search, 'user_search.graphql')
+ANIME_SEARCH_QUERY = read_text(search, "anime_search.graphql")
+MANGA_SEARCH_QUERY = read_text(search, "manga_search.graphql")
+CHARACTER_SEARCH_QUERY = read_text(search, "character_search.graphql")
+STAFF_SEARCH_QUERY = read_text(search, "staff_search.graphql")
+USER_SEARCH_QUERY = read_text(search, "user_search.graphql")
 
-ANIME_GET_QUERY = read_text(get, 'anime_get.graphql')
-MANGA_GET_QUERY = read_text(get, 'manga_get.graphql')
-CHARACTER_GET_QUERY = read_text(get, 'character_get.graphql')
-STAFF_GET_QUERY = read_text(get, 'staff_get.graphql')
-USER_GET_QUERY = read_text(get, 'user_get.graphql')
-LIST_GET_QUERY = read_text(get, 'list_get.graphql')
-LIST_ITEM_GET_QUERY = read_text(get, 'list_item_get.graphql')
+ANIME_GET_QUERY = read_text(get, "anime_get.graphql")
+MANGA_GET_QUERY = read_text(get, "manga_get.graphql")
+CHARACTER_GET_QUERY = read_text(get, "character_get.graphql")
+STAFF_GET_QUERY = read_text(get, "staff_get.graphql")
+USER_GET_QUERY = read_text(get, "user_get.graphql")
+LIST_GET_QUERY = read_text(get, "list_get.graphql")
+LIST_ITEM_GET_QUERY = read_text(get, "list_item_get.graphql")
 
 # I wonder if the activity queries should be lumped under "get" instead.
 # They function pretty much exactly the same. - nfitzen
 
-LIST_ACTIVITY_QUERY = read_text(activity, 'list_activity.graphql')
-TEXT_ACTIVITY_QUERY = read_text(activity, 'text_activity.graphql')
-MESSAGE_ACTIVITY_QUERY = read_text(activity, 'message_activity.graphql')
-MESSAGE_ACTIVITY_QUERY_SENT = read_text(activity, 'message_activity_sent.graphql')
+LIST_ACTIVITY_QUERY = read_text(activity, "list_activity.graphql")
+TEXT_ACTIVITY_QUERY = read_text(activity, "text_activity.graphql")
+MESSAGE_ACTIVITY_QUERY = read_text(activity, "message_activity.graphql")
+MESSAGE_ACTIVITY_QUERY_SENT = read_text(activity, "message_activity_sent.graphql")
 MESSAGE_ACTIVITY_SENT_QUERY = MESSAGE_ACTIVITY_QUERY_SENT
