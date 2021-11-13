caeec0478f74579bf807d8b590938f6b4d25f726
Nathaniel Fitzenrider <nathaniel.fitzenrider@gmail.com>
2021-11-13 02:07:52 -0500

    Add Python 3.6 support
    It's completely untested, though. I hope to change that.

--- anilist/queries/__init__.py
+++ anilist/queries/__init__.py
@@ -2,8 +2,40 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2021 Amano Team <https://amanoteam.com/> and the python-anilist contributors
 
-from importlib.resources import read_text
 from ._query_files import activity, get, search
+
+try: # because we're supposed to support Python 3.6. I don't get it, but ok.
+    from importlib.resources import read_text
+except ImportError: # re-implement some importlib stuff
+    from typing import TYPE_CHECKING, Union
+
+    if TYPE_CHECKING:
+        from types import ModuleType
+        from os import PathLike
+        Package = Union[str, ModuleType]
+        Resource = Union[str, PathLike]
+
+    from pkgutil import get_data
+
+    def read_text(package: Package,
+                  resource: str,
+                  encoding: str = 'utf-8',
+                  errors: str = 'strict') -> str:
+        """Return the decoded string of the resource.
+
+        The decoding-related arguments have the same semantics as those of
+        bytes.decode().
+        """
+        if isinstance(package, str):
+            data = get_data(package=package, resource=resource)
+        else:
+            data = get_data(package=package.__name__, resource=resource)
+
+        if data is None:
+            raise ImportError("Cannot find module or improper loader.")
+
+        return data.decode(encoding=encoding, errors=errors)
+
 
 __all__ = (
     "ANIME_SEARCH_QUERY",
