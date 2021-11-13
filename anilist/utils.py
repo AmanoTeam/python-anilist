a8484a0bdc8d19224587f1ee942046157927e59c
Nathaniel Fitzenrider <nathaniel.fitzenrider@gmail.com>
2021-11-12 23:07:25 -0500

    Remove import * from anilist.utils

--- anilist/utils.py
+++ anilist/utils.py
@@ -2,7 +2,25 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2021 Amano Team <https://amanoteam.com/> and the python-anilist contributors
 
-from .queries import *
+from .queries import (
+    ANIME_SEARCH_QUERY,
+    MANGA_SEARCH_QUERY,
+    CHARACTER_SEARCH_QUERY,
+    STAFF_SEARCH_QUERY,
+    USER_SEARCH_QUERY,
+    ANIME_GET_QUERY,
+    MANGA_GET_QUERY,
+    CHARACTER_GET_QUERY,
+    STAFF_GET_QUERY,
+    USER_GET_QUERY,
+    LIST_GET_QUERY,
+    LIST_ITEM_GET_QUERY,
+    LIST_ACTIVITY_QUERY,
+    TEXT_ACTIVITY_QUERY,
+    MESSAGE_ACTIVITY_QUERY,
+    MESSAGE_ACTIVITY_QUERY_SENT,
+    MESSAGE_ACTIVITY_SENT_QUERY,
+)
 
 API_URL = "https://graphql.anilist.co"
 HEADERS = {
